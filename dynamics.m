9316e6c5ef889c722a537ec56d8d10f6deac9849
basel <basel@umich.edu>
2018-12-05 16:17:13 -0500

    forward backward passes

--- dynamics.m
+++ dynamics.m
@@ -11,12 +11,7 @@ Q_n = diag([1 1 1 1 1 1]);
 Q_N = Q_n;
 R = diag([1 1 1]);
 
-% initial state
-% theta = 0 is straight down. theta positive is counter clockwise.
-x_0 = [pi/2 ; 0 ; 0 ; 0 ; 0 ; 0];
-u_0 = [0 ; 0 ; 0];
-x_1 = [pi/2 ; 0 ; 0 ; 0 ; 0 ; 0];
-u_1 = [0 ; 0 ; 0];
+% theta = 0 is straight down. theta positive is clockwise.
 
 % get symbolic derivatives
 [x, u, x_dot_nonlin_sym] = get_dyn2();
@@ -28,28 +23,30 @@ x_dot_nonlin = @(x_eval, u_eval) double(subs(x_dot_nonlin_sym,  [x ; u], [x_eval
 x_dot_lin = @(x_about, u_about, x_eval, u_eval) ...
     double(subs(taylor(x_dot_nonlin_sym, [x ; u], [x_about ; u_about],'Order', 2), ...
     [x ; u], [x_eval ; u_eval]));
-A = @(x_about, u_about) double(subs(A_sym, [x ; u], [x_about ; u_about]));
+A_cont = @(x_about, u_about) double(subs(A_sym, [x ; u], [x_about ; u_about]));
 B = @(x_about, u_about) double(subs(B_sym, [x ; u], [x_about ; u_about]));
+A = @(x_about, u_about) eye(6) + dt*A_cont(x_about, u_about);
 
 % usage
 % remember, linearize about the nominal trajectory
-x_dot_lin(x_0, u_0, x_0, u_0);
-x_dot_nonlin_0 = x_dot_nonlin(x_0, u_0);
-A_0 = A(x_0, u_0);
-B_0 = B(x_0, u_0);
+% x_dot_lin(x_0, u_0, x_0, u_0);
+% x_dot_nonlin_0 = x_dot_nonlin(x_0, u_0);
+% A_0 = A(x_0, u_0);
+% B_0 = B(x_0, u_0);
 
 % generate desired trajectory
-x_nominal = zeros(N, 6);
-u_nominal = zeros(N, 3);
-x_nominal(1, :) = [pi ; pi ; pi+0.01 ; 0 ; 0 ; 0];
+x_nominal = zeros(6, N);
+u_nominal = zeros(3, N);
+x_nominal(:, 1) = [pi ; pi ; pi+0.01 ; 0 ; 0 ; 0];
 x_desired = [0 ; -pi/2 ; -pi/2 ; 0 ; 0 ; 0];
-Kp = [0.1*eye(3) 0.5*eye(3)];
+% Kp = [0.1*eye(3) 0.5*eye(3)];
 for i = 1:N-1
-    control = -Kp * (x_desired - x_nominal(i, :)');
-    control(control > 10) = 10; control(control < -10) = -10;
+    disp(i)
+    % control = -Kp * (x_desired - x_nominal(i, :)');
+    % control(control > 10) = 10; control(control < -10) = -10;
     % u_nominal(i, :) = control;
-    der = x_dot_nonlin(x_nominal(i, :)', u_nominal(i, :)')';
-    x_nominal(i+1, :) = x_nominal(i,:) + dt*der;
+    der = x_dot_nonlin(x_nominal(:, i), u_nominal(:, i));
+    x_nominal(:, i+1) = x_nominal(:, i) + dt*der;
 end
 
 % nonlinear constraint
@@ -57,56 +54,96 @@ end
 % this also applies at N! g3(x_N,N) = g2(x_n,n)
 % ee is end effector
 ee_start = [1 ; -1];
-ee_end = [2 ; 2];
+ee_end = [1 ; 1];
 g2 = @(x_n, n) [ee_start(1) + (ee_end(1) - ee_start(1))*(n-1)/(N-1) - ...
-    (L1*cos(x_n(1)) + L2*cos(x_n(1)+x_n(2)) + L3*cos(x_n(1)+x_n(2)+x_n(3)) ...
-    + L4*cos(x_n(1)+x_n(2)+x_n(3)+x_n(4))) ; ...
+    (L1*cos(x_n(1)) + L2*cos(x_n(2)) + L3*cos(x_n(3))); ...
     ee_start(2) + (ee_end(2) - ee_start(2))*(n-1)/(N-1) - ...
-    L1*sin(x_n(1)) + L2*sin(x_n(1)+x_n(2)) + L3*sin(x_n(1)+x_n(2)+x_n(3)) ...
-    + L4*sin(x_n(1)+x_n(2)+x_n(3)+x_n(4))];
+    (L1*sin(x_n(1)) + L2*sin(x_n(2)) + L3*sin(x_n(3)))];
 
 % linearized constraint
 % remember to linearize about the nominal trajectory!
 % the x_n passed in here should come from the nominal trajectory
 % apply to C_N and d_N also
-C_n = @(x_n) [L1*sin(x_n(1))+L2*sin(x_n(1)+x_n(2))+L3*sin(x_n(1)+x_n(2)+x_n(3))+L4*sin(x_n(1)+x_n(2)+x_n(3)+x_n(4)) ...
-              L2*sin(x_n(1)+x_n(2))+L3*sin(x_n(1)+x_n(2)+x_n(3))+L4*sin(x_n(1)+x_n(2)+x_n(3)+x_n(4)) ...
-              L3*sin(x_n(1)+x_n(2)+x_n(3))+L4*sin(x_n(1)+x_n(2)+x_n(3)+x_n(4)) ...
-              L4*sin(x_n(1)+x_n(2)+x_n(3)+x_n(4)) ...
-              0 0 0 0 ; ...
-             -L1*cos(x_n(1))-L2*cos(x_n(1)+x_n(2))-L3*cos(x_n(1)+x_n(2)+x_n(3))-L4*cos(x_n(1)+x_n(2)+x_n(3)+x_n(4)) ...
-             -L2*cos(x_n(1)+x_n(2))-L3*cos(x_n(1)+x_n(2)+x_n(3))-L4*cos(x_n(1)+x_n(2)+x_n(3)+x_n(4)) ...
-             -L3*cos(x_n(1)+x_n(2)+x_n(3))-L4*cos(x_n(1)+x_n(2)+x_n(3)+x_n(4)) ...
-             -L4*cos(x_n(1)+x_n(2)+x_n(3)+x_n(4)) ...
-              0 0 0 0];
+C_n = @(x_n) [L1*sin(x_n(1))+L2*sin(x_n(2))+L3*sin(x_n(3)) ...
+              L2*sin(x_n(2))+L3*sin(x_n(3)) ...
+              L3*sin(x_n(3)) ...
+              0 0 0 ; ...
+             -L1*cos(x_n(1))-L2*cos(x_n(2))-L3*cos(x_n(3)) ...
+             -L2*cos(x_n(2))-L3*cos(x_n(3)) ...
+             -L3*cos(x_n(3)) ...
+              0 0 0];
 d_n = [0 ; 0];
 
-M_n = C_n(x_1) * A(x_1,u_1) * B(x_0,u_0);
-N_n = C_n(x_1) * A(x_1,u_1) * A(x_0,u_0);
-
-% eqn 13
-w = null(M_n);
-P = w*inv(w'*w)*w';
-
-% eqn 15
-epsilon_n = pinv(M_n)*d_n;
-U_n = pinv(M_n)*N_n;
-
-% eqn 17
-A_tilde_n = A_0 + B_0*U_n;
-B_tilde_n = B_0 * P;
-g_tilde_n = B_0 * epsilon_n;
-
-q_tilde_n = epsilon_n' * R * epsilon_n/2; % 20
-q_bold_tilde_n = U_n' * R * epsilon_n; % 21
-Q_tilde_n = Q + U_n' * R * U_n; % 22
-r_tilde_n = P*R*epsilon_n; % 23
-R_tilde_n = P*R*P; % 24
-P_tilde_n = P*R*U_n; % 25
+% begin forward pass
+A_n = zeros(6,6,N);
+B_n = zeros(6,3,N);
+M_n = zeros(2,3,N);
+N_n = zeros(2,6,N);
+Proj_n = zeros(3,3,N);
+epsilon_n = zeros(3,1,N);
+U_n = zeros(3,6,N);
+A_tilde_n = zeros(6,6,N);
+B_tilde_n = zeros(6,3,N);
+g_tilde_n = zeros(6,1,N);
+q_tilde_n = zeros(1,1,N);
+q_bold_tilde_n = zeros(6,1,N);
+Q_tilde_n = zeros(6,6,N);
+r_tilde_n = zeros(3,1,N);
+R_tilde_n = zeros(3,3,N);
+P_tilde_n = zeros(3,6,N);
+A_n(:,:,N) = A(x_nominal(:,N), u_nominal(:,N));
+B_n(:,:,N) = B(x_nominal(:,N), u_nominal(:,N));
+for n = N-1:-1:1
+    disp(n)
+    A_n(:,:,n) = A(x_nominal(:, n), u_nominal(:, n));
+    B_n(:,:,n) = B(x_nominal(:, n), u_nominal(:, n));
+    
+    M_n(:,:,n) = C_n(x_nominal(:, n+1)) * A_n(:,:,n+1) * B_n(:,:,n);
+    N_n(:,:,n) = C_n(x_nominal(:, n+1)) * A_n(:,:,n+1) * A_n(:,:,n);
+    
+    % eqn 13
+    w = null(M_n(:,:,n));
+    Proj_n(:,:,n) = w*inv(w'*w)*w';
+    
+    % eqn 15
+    epsilon_n(:,:,n) = pinv(M_n(:,:,n))*d_n;
+    U_n(:,:,n) = pinv(M_n(:,:,n))*N_n(:,:,n);
+    
+    % eqn 17
+    A_tilde_n(:,:,n) = A_n(:,:,n) + B_n(:,:,n)*U_n(:,:,n);
+    B_tilde_n(:,:,n) = B_n(:,:,n) * Proj_n(:,:,n);
+    g_tilde_n(:,:,n) = B_n(:,:,n) * epsilon_n(:,:,n);
+    
+    q_tilde_n(:,:,n) = epsilon_n(:,:,n)' * R * epsilon_n(:,:,n)/2; % 20
+    q_bold_tilde_n(:,:,n) = U_n(:,:,n)' * R * epsilon_n(:,:,n); % 21
+    Q_tilde_n(:,:,n) = Q_n + U_n(:,:,n)' * R * U_n(:,:,n); % 22
+    r_tilde_n(:,:,n) = Proj_n(:,:,n)*R*epsilon_n(:,:,n); % 23
+    R_tilde_n(:,:,n) = Proj_n(:,:,n)*R*Proj_n(:,:,n); % 24
+    P_tilde_n(:,:,n) = Proj_n(:,:,n)*R*U_n(:,:,n); % 25
+end
 
 % big S is S2 (8x8)
 % little s bold is S1 (8x1)
 % little s is S0 (scalar)
 
 % eqn 29
-
+S2 = zeros(6,6,N);
+S1 = zeros(6,1,N);
+S0 = zeros(N,1);
+S2(:,:,N) = Q_N;
+S1(:,N) = zeros(6,1);
+S0(N) = 0;
+for n = N-1:-1:2
+    disp(n)
+    h = r_tilde_n(:,:,n) + B_tilde_n(:,:,n)' * (S1(:,n+1)+ S2(:,:,n+1)*g_tilde_n(:,:,n));
+    G = P_tilde_n(:,:,n) + B_tilde_n(:,:,n)' * S2(:,:,n+1) * A_tilde_n(:,:,n);
+    H = R_tilde_n(:,:,n) + B_tilde_n(:,:,n)' * S2(:,:,n+1) * B_tilde_n(:,:,n);
+    l = -pinv(H)*h;
+    L = -pinv(H)*G;
+    
+    S2(:,:,n) = Q_tilde_n(:,:,n) + A_tilde_n(:,:,n)'*S2(:,:,n+1)*A_tilde_n(:,:,n) - L'*H*L;
+    S1(:,n) = q_bold_tilde_n(:,:,n) + A_tilde_n(:,:,n)' * ( S1(:,n+1) + S2(:,:,n+1) * g_tilde_n(:,:,n) ) + ...
+        G'*l + L' * (h+H*l);
+    S0(n) = q_tilde_n(:,:,n) + S0(n+1) + g_tilde_n(:,:,n)'*S1(:,n+1) + (1/2)*g_tilde_n(:,:,n)'*S2(:,:,n+1)*g_tilde_n(:,:,n) ...
+        + l' * (h + (1/2)*H*l);
+end
